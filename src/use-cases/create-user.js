1ce79f7ab6978ba4f77fe002fd8887f4da442690
José Leonardo <joseleonaordo85@gnail.com>
2025-04-30 09:25:47 -0300

    feat: add CreateUserUseCase

---
+++ src/use-cases/create-user.js
@@ -0,0 +1,28 @@
+import { v4 as uuidv4 } from 'uuid'
+import bycrypt from 'bcrypt'
+import PostgresCreateUserRepository from '../repositories/postgres/create-user.js'
+
+export class CreateUserUseCase {
+    async execute(createUserParams) {
+        // TODO: verificar se o email já está em uso
+
+        // gerar ID do usuárionpm i uuid
+        const userId = uuidv4()
+
+        // criptografar senha
+        const hashedPassword = await bycrypt.hash(createUserParams.password, 10)
+
+        // inserir o usuário no banco de dados
+        const user = {
+            ...createUserParams,
+            id: userId,
+            password: hashedPassword,
+        }
+
+        const postgresCreateUserRepository = new PostgresCreateUserRepository()
+
+        const createdUser = await postgresCreateUserRepository.execute(user)
+
+        return createdUser
+    }
+}
