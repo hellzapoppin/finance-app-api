b48700c0b63ba990651e937e596fad37ab0e703a
José Leonardo <joseleonaordo85@gnail.com>
2025-05-02 14:11:53 -0300

    feat: add CreateTransactionUseCase

---
+++ src/use-cases/transaction/create-transaction.js
@@ -0,0 +1,31 @@
+import { v4 as uuidv4 } from 'uuid'
+import { UserNotFoundError } from '../../errors/user.js'
+
+export class CreateTransactionUseCase {
+    constructor(createTransactionRepository, getUserByIdRepository) {
+        this.createTransactionRepository = createTransactionRepository
+        this.getUserByIdRepository = getUserByIdRepository
+    }
+
+    async execute(createTransactionParams) {
+        const { userId } = createTransactionParams.userId
+
+        const user = await this.getUserByIdRepository.execute(userId)
+
+        if (!user) {
+            throw new UserNotFoundError(userId)
+        }
+
+        const transactionId = uuidv4()
+
+        const transaction = {
+            id: transactionId,
+            ...createTransactionParams,
+        }
+
+        const createdTransaction =
+            await this.createTransactionRepository.execute(transaction)
+
+        return createdTransaction
+    }
+}
