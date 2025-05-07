06b8048fbc0e6eeeb3a312b38530f4b023dd1dc2
José Leonardo <joseleonaordo85@gnail.com>
2025-05-07 15:57:04 -0300

    feat: ensure GetTransactionByUserIdRepository and UpdateTransactionRepository use Prisma

--- src/repositories/postgres/transaction/get-transactions-by-user-id.js
+++ src/repositories/postgres/transaction/get-transactions-by-user-id.js
@@ -1,13 +1,22 @@
-import { PostgresHelper } from '../../../db/postgres/helpers.js'
+// criando inserção no banco sem o ORM Prisma
+// import { PostgresHelper } from '../../../db/postgres/helpers.js'
+
+import { prisma } from '../../../../prisma/prisma.js'
 
 export class PostgresGetTransactionsByUserIdRepository {
     async execute(userId) {
-        const transactions = await PostgresHelper.query(
-            `SELECT * FROM transactions
-            WHERE user_id = $1`,
-            [userId],
-        )
+        return await prisma.transaction.findMany({
+            where: {
+                user_id: userId,
+            },
+        })
+        // criando inserção no banco sem o ORM Prisma
+        // const transactions = await PostgresHelper.query(
+        //     `SELECT * FROM transactions
+        //     WHERE user_id = $1`,
+        //     [userId],
+        // )
 
-        return transactions
+        // return transactions
     }
 }
--- src/repositories/postgres/transaction/update-transaction.js
+++ src/repositories/postgres/transaction/update-transaction.js
@@ -1,29 +1,37 @@
-import { PostgresHelper } from '../../../db/postgres/helpers.js'
+// criando inserção no banco sem o ORM Prisma
+// import { PostgresHelper } from '../../../db/postgres/helpers.js'
+import { prisma } from '../../../../prisma/prisma.js'
 
 export class PostgresUpdateTransactionRepository {
     async execute(transactionId, updateTransactionParams) {
-        const updateFields = []
-        const updateValues = []
-
-        Object.keys(updateTransactionParams).forEach((key) => {
-            updateFields.push(`${key} = $${updateFields.length + 1}`)
-            updateValues.push(updateTransactionParams[key])
+        return await prisma.transaction.update({
+            where: { id: transactionId },
+            data: updateTransactionParams,
         })
 
-        updateValues.push(transactionId)
+        // criando inserção no banco sem o ORM Prisma
+        // const updateFields = []
+        // const updateValues = []
 
-        const updateQuery = `
-            UPDATE transactions
-            SET ${updateFields.join(', ')}
-            WHERE id = $${updateValues.length}
-            RETURNING *;
-        `
+        // Object.keys(updateTransactionParams).forEach((key) => {
+        //     updateFields.push(`${key} = $${updateFields.length + 1}`)
+        //     updateValues.push(updateTransactionParams[key])
+        // })
 
-        const updatedTransaction = await PostgresHelper.query(
-            updateQuery,
-            updateValues,
-        )
+        // updateValues.push(transactionId)
 
-        return updatedTransaction[0]
+        // const updateQuery = `
+        //     UPDATE transactions
+        //     SET ${updateFields.join(', ')}
+        //     WHERE id = $${updateValues.length}
+        //     RETURNING *;
+        // `
+
+        // const updatedTransaction = await PostgresHelper.query(
+        //     updateQuery,
+        //     updateValues,
+        // )
+
+        // return updatedTransaction[0]
     }
 }
