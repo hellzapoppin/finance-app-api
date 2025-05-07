7b28bb3d0600eda47f52aea4a8b0e413f7264530
José Leonardo <joseleonaordo85@gnail.com>
2025-05-07 15:31:55 -0300

    feat: ensure CreateTransactionRepository and DeleteTransactionRepository use Prisma

--- src/repositories/postgres/transaction/create-transaction.js
+++ src/repositories/postgres/transaction/create-transaction.js
@@ -1,23 +1,31 @@
-import { PostgresHelper } from '../../../db/postgres/helpers.js'
+// criando inserção no banco sem o ORM Prisma
+// import { PostgresHelper } from '../../../db/postgres/helpers.js'
+
+import { prisma } from '../../../../prisma/prisma.js'
 
 export class PostgresCreateTransactionRepository {
     async execute(createTransactionParams) {
-        const createdTransaction = await PostgresHelper.query(
-            `
-            INSERT INTO transactions (id, user_id, name, date, amount, type)
-            VALUES ($1, $2, $3, $4, $5, $6)
-            RETURNING *;
-            `,
-            [
-                createTransactionParams.id,
-                createTransactionParams.user_id,
-                createTransactionParams.name,
-                createTransactionParams.date,
-                createTransactionParams.amount,
-                createTransactionParams.type,
-            ],
-        )
+        return await prisma.transaction.create({
+            data: createTransactionParams,
+        })
 
-        return createdTransaction[0]
+        // criando inserção no banco sem o ORM Prisma
+        // const createdTransaction = await PostgresHelper.query(
+        //     `
+        //     INSERT INTO transactions (id, user_id, name, date, amount, type)
+        //     VALUES ($1, $2, $3, $4, $5, $6)
+        //     RETURNING *;
+        //     `,
+        //     [
+        //         createTransactionParams.id,
+        //         createTransactionParams.user_id,
+        //         createTransactionParams.name,
+        //         createTransactionParams.date,
+        //         createTransactionParams.amount,
+        //         createTransactionParams.type,
+        //     ],
+        // )
+
+        // return createdTransaction[0]
     }
 }
--- src/repositories/postgres/transaction/delete-transaction.js
+++ src/repositories/postgres/transaction/delete-transaction.js
@@ -1,14 +1,27 @@
-import { PostgresHelper } from '../../../db/postgres/helpers.js'
+// criando inserção no banco sem o ORM Prisma
+// import { PostgresHelper } from '../../../db/postgres/helpers.js'
+
+import { prisma } from '../../../../prisma/prisma.js'
 
 export class PostgresDeleteTransactionRepository {
     async execute(transactionId) {
-        const deletedTrasaction = await PostgresHelper.query(
-            `DELETE FROM transactions
-            WHERE id = $1
-            RETURNING *`,
-            [transactionId],
-        )
+        try {
+            return await prisma.transaction.delete({
+                where: { id: transactionId },
+            })
+        } catch (error) {
+            console.log(error)
+            return null
+        }
 
-        return deletedTrasaction[0]
+        // criando inserção no banco sem o ORM Prisma
+        // const deletedTrasaction = await PostgresHelper.query(
+        //     `DELETE FROM transactions
+        //     WHERE id = $1
+        //     RETURNING *`,
+        //     [transactionId],
+        // )
+
+        // return deletedTrasaction[0]
     }
 }
