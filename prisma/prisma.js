0bebbbbd3f19e6edbc976a3cf18f29ba68f81627
José Leonardo <joseleonaordo85@gnail.com>
2025-05-08 09:43:36 -0300

    chore: add prisma generate to packeage.json

--- prisma/prisma.js
+++ prisma/prisma.js
@@ -1,3 +1,4 @@
-import { PrismaClient } from '../src/generated/prisma/index.js'
+// import { PrismaClient } from '../src/generated/prisma/index.js'
+import { PrismaClient } from '@prisma/client'
 
 export const prisma = new PrismaClient()
