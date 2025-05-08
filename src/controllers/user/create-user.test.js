94ef508044905c9c2db0659641b4e9d1e38fdeac
José Leonardo <joseleonaordo85@gnail.com>
2025-05-08 14:21:51 -0300

    test: ensure 400 is return if email is not provided

--- src/controllers/user/create-user.test.js
+++ src/controllers/user/create-user.test.js
@@ -67,4 +67,21 @@ describe('Create User Controller', () => {
         //assert
         expect(result.statusCode).toBe(400)
     })
+
+    it('should return 400 if email is not provided', async () => {
+        const createUserUseCase = new CreateUserUseCaseStub()
+        const createUserController = new CreateUserController(createUserUseCase)
+
+        const httpRequest = {
+            body: {
+                first_name: 'José',
+                last_name: 'Leonardo',
+                password: '123456',
+            },
+        }
+
+        const result = await createUserController.execute(httpRequest)
+
+        expect(result.statusCode).toBe(400)
+    })
 })
