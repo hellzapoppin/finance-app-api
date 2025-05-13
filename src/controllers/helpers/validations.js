3714901c93bd049b3b31419a26041c46d13ccafa
José Leonardo <joseleonaordo85@gnail.com>
2025-05-13 10:37:14 -0300

    refactor: remove unused code

--- src/controllers/helpers/validations.js
+++ src/controllers/helpers/validations.js
@@ -10,26 +10,3 @@ export const requiredFieldsIsMissingResponse = (field) =>
     badRequest({
         message: `The field ${field} is required`,
     })
-
-export const checkIfIsString = (value) => typeof value === 'string'
-
-export const validateRequiredFields = (params, requiredFields) => {
-    for (const field of requiredFields) {
-        const fieldIsMissing = !params[field]
-        const fieldIsEmpty =
-            checkIfIsString(params[field]) &&
-            validator.isEmpty(params[field], { ignore_whitespace: true })
-
-        if (fieldIsMissing || fieldIsEmpty) {
-            return {
-                missingField: field,
-                ok: false,
-            }
-        }
-    }
-
-    return {
-        missingField: undefined,
-        ok: true,
-    }
-}
