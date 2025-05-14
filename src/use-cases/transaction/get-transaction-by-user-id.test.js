789313e1163fab59343f1cfe257b55f7e8b4f625
José Leonardo <joseleonaordo85@gnail.com>
2025-05-14 16:03:03 -0300

    test: ensure GetTransactionsByUserIdUseCase get transactions by user ID successfully

---
+++ src/use-cases/transaction/get-transaction-by-user-id.test.js
@@ -0,0 +1,58 @@
+import { faker } from '@faker-js/faker'
+import { GetTransactionsByUserIdUseCase } from './get-transactions-by-user-id.js'
+
+describe('Get Transaction By User Id', () => {
+    const userId = faker.string.uuid()
+
+    const transactions = [
+        {
+            id: faker.string.uuid(),
+            name: faker.book.title(),
+            date: faker.date.anytime().toISOString(),
+            amount: Number(faker.finance.amount()),
+            type: faker.helpers.arrayElements([
+                'EARNING',
+                'EXPENSE',
+                'INVESTMENT',
+            ]),
+        },
+    ]
+
+    class GetUserByIdRepositoryStub {
+        async execute(userId) {
+            return {
+                id: userId,
+                fist_name: faker.person.firstName(),
+                last_name: faker.person.lastName(),
+                email: faker.internet.email(),
+                password: faker.internet.password({ length: 7 }),
+            }
+        }
+    }
+
+    class GetTransactionsByUserIdRepositoryStub {
+        async execute(userId) {
+            return [{ ...transactions, user_id: userId }]
+        }
+    }
+
+    const makeSut = () => {
+        const getTransactionsByUserIdRepository =
+            new GetTransactionsByUserIdRepositoryStub()
+        const getUserByIdRepository = new GetUserByIdRepositoryStub()
+        const sut = new GetTransactionsByUserIdUseCase(
+            getTransactionsByUserIdRepository,
+            getUserByIdRepository,
+        )
+
+        return { sut, getTransactionsByUserIdRepository, getUserByIdRepository }
+    }
+
+    it('should get transactions by user ID successfully', async () => {
+        const { sut } = makeSut()
+
+        const result = await sut.execute(userId)
+
+        expect(result).toEqual([{ ...transactions, user_id: userId }])
+    })
+})
