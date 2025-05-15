747d233a12a0d901bb73ec8c649f9774ba28148a
José Leonardo <joseleonaordo85@gnail.com>
2025-05-15 11:19:37 -0300

    test: add user balance fixtures

--- src/tests/fixtures/user.js
+++ src/tests/fixtures/user.js
@@ -1,9 +1,15 @@
 import { faker } from '@faker-js/faker'
 
 export const user = {
-    id: faker.string.uuid(),
     fist_name: faker.person.firstName(),
     last_name: faker.person.lastName(),
     email: faker.internet.email(),
     password: faker.internet.password({ length: 7 }),
+}
+
+export const userBalance = {
+    earnings: Number(faker.finance.amount()),
+    expenses: Number(faker.finance.amount()),
+    investments: Number(faker.finance.amount()),
+    balance: Number(faker.finance.amount()),
 }
