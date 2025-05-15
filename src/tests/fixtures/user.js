9327fc19657594ed5ab3307f631ea29d156f6170
José Leonardo <joseleonaordo85@gnail.com>
2025-05-15 14:21:18 -0300

    fix: first_name typo

--- src/tests/fixtures/user.js
+++ src/tests/fixtures/user.js
@@ -1,7 +1,8 @@
 import { faker } from '@faker-js/faker'
 
 export const user = {
-    fist_name: faker.person.firstName(),
+    id: faker.string.uuid(),
+    first_name: faker.person.firstName(),
     last_name: faker.person.lastName(),
     email: faker.internet.email(),
     password: faker.internet.password({ length: 7 }),
