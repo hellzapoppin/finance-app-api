893ec03ee0412bac4b76ad692ad5d6893c664b3c
José Leonardo <joseleonaordo85@gnail.com>
2025-05-16 14:35:06 -0300

    test: ensure PostgresCreateUserRepository throws if Prisma throws

--- src/repositories/postgres/user/create-user.test.js
+++ src/repositories/postgres/user/create-user.test.js
@@ -23,4 +23,13 @@ describe('Create User Repository', () => {
 
         expect(primaSpy).toHaveBeenLastCalledWith({ data: user })
     })
+
+    it('should throw if Prisma throws', async () => {
+        const sut = new PostgresCreateUserRepository()
+        jest.spyOn(prisma.user, 'create').mockRejectedValueOnce(new Error())
+
+        const promise = sut.execute(user)
+
+        expect(promise).rejects.toThrow()
+    })
 })
