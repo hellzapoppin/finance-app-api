a97b81f83fcef692f9de0e6cca9fcb6fa979782a
José Leonardo <joseleonaordo85@gnail.com>
2025-05-19 15:05:39 -0300

    feat: ensure PostgresDeleteTransactionRepository throws if Prisma throws

--- src/repositories/postgres/transaction/delete-transaction.js
+++ src/repositories/postgres/transaction/delete-transaction.js
@@ -1,7 +1,9 @@
 // criando inserção no banco sem o ORM Prisma
 // import { PostgresHelper } from '../../../db/postgres/helpers.js'
 
+import { PrismaClientKnownRequestError } from '@prisma/client/runtime/library'
 import { prisma } from '../../../../prisma/prisma.js'
+import { TransactionNotFoundError } from '../../../errors/transaction.js'
 
 export class PostgresDeleteTransactionRepository {
     async execute(transactionId) {
@@ -11,7 +13,12 @@ export class PostgresDeleteTransactionRepository {
             })
         } catch (error) {
             console.log(error)
-            return null
+            if (error instanceof PrismaClientKnownRequestError) {
+                if (error.code === 'P2025') {
+                    throw new TransactionNotFoundError(transactionId)
+                }
+            }
+            throw error
         }
 
         // criando inserção no banco sem o ORM Prisma
