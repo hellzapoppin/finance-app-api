8278517a9c1e6b0b847d237da8a7255b00c4ed41
José Leonardo <joseleonaordo85@gnail.com>
2025-05-20 07:50:07 -0300

    test: ensure makeDeleteTransactionController returns valid DeleteTransactionController

--- src/factories/controllers/transaction.test.js
+++ src/factories/controllers/transaction.test.js
@@ -1,9 +1,11 @@
 import {
     CreateTransactionController,
+    DeleteTransactionController,
     UpdateTransactionController,
 } from '../../controllers'
 import {
     makeCreateTransactionController,
+    makeDeleteTransactionController,
     makeUpdateTransactionController,
 } from './transaction'
 
@@ -17,6 +19,12 @@ describe('Transaction Controller Factories', () => {
     it('should return a valid UpdateTrasactionController instance', () => {
         expect(makeUpdateTransactionController()).toBeInstanceOf(
             UpdateTransactionController,
+        )
+    })
+
+    it('should return a valid DeleteTrasactionController instance', () => {
+        expect(makeDeleteTransactionController()).toBeInstanceOf(
+            DeleteTransactionController,
         )
     })
 })
