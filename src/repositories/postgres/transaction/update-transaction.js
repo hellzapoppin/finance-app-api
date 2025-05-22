f9a98dafd25ad78541214cf7cd0f6ce640efce93
José Leonardo <joseleonaordo85@gnail.com>
2025-05-22 07:57:52 -0300

    feat: ensure PostgresUpdateTransactionRepository throws TransactionNotFoundError if prisma throws

--- src/repositories/postgres/transaction/update-transaction.js
+++ src/repositories/postgres/transaction/update-transaction.js
@@ -1,13 +1,25 @@
 // criando inserção no banco sem o ORM Prisma
 // import { PostgresHelper } from '../../../db/postgres/helpers.js'
+import { PrismaClientKnownRequestError } from '@prisma/client/runtime/library.js'
 import { prisma } from '../../../../prisma/prisma.js'
+import { TransactionNotFoundError } from '../../../errors/transaction.js'
 
 export class PostgresUpdateTransactionRepository {
     async execute(transactionId, updateTransactionParams) {
-        return await prisma.transaction.update({
-            where: { id: transactionId },
-            data: updateTransactionParams,
-        })
+        try {
+            return await prisma.transaction.update({
+                where: { id: transactionId },
+                data: updateTransactionParams,
+            })
+        } catch (error) {
+            console.log(error)
+            if (error instanceof PrismaClientKnownRequestError) {
+                if (error.code === 'P2025') {
+                    throw new TransactionNotFoundError(transactionId)
+                }
+            }
+            throw error
+        }
 
         // criando inserção no banco sem o ORM Prisma
         // const updateFields = []
