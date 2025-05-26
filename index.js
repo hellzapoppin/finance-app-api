14b67c5462435d6bcda3360a97c6ea9ae8094d24
José Leonardo <joseleonaordo85@gnail.com>
2025-05-26 14:13:22 -0300

    ci: fix test run on github actions

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 import 'dotenv/config'
 
-import { app } from './src/app.js'
+import { app } from './src/app.cjs'
 
 app.listen(process.env.PORT, () =>
     console.log(`Server is running on port ${process.env.PORT}`),
