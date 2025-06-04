f438d21a5c4dd463250c19fff6efa4e7bc9e9449
José Leonardo <joseleonaordo85@gnail.com>
2025-06-04 10:26:32 -0300

    feat: ensure GetUserBalanceRepository receives to and from queries

--- src/repositories/postgres/user/get-user-balance.js
+++ src/repositories/postgres/user/get-user-balance.js
@@ -6,13 +6,20 @@ import { prisma } from '../../../../prisma/prisma.js'
 import { TransactionType } from '@prisma/client'
 
 export class PostgresGetUserBalanceRepository {
-    async execute(userId) {
+    async execute(userId, from, to) {
+        const dateFilter = {
+            date: {
+                gte: new Date(from),
+                lte: new Date(to),
+            },
+        }
         const {
             _sum: { amount: totalExpenses },
         } = await prisma.transaction.aggregate({
             where: {
                 user_id: userId,
                 type: TransactionType.EXPENSE,
+                ...dateFilter,
             },
             _sum: {
                 amount: true,
@@ -25,6 +32,7 @@ export class PostgresGetUserBalanceRepository {
             where: {
                 user_id: userId,
                 type: TransactionType.EARNING,
+                ...dateFilter,
             },
             _sum: {
                 amount: true,
@@ -37,6 +45,7 @@ export class PostgresGetUserBalanceRepository {
             where: {
                 user_id: userId,
                 type: TransactionType.INVESTMENT,
+                ...dateFilter,
             },
             _sum: {
                 amount: true,
