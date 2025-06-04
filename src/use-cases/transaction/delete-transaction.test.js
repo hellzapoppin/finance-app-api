f76c17cad3a0332d18bafbc0f840bdc7350bd8a9
José Leonardo <joseleonaordo85@gnail.com>
2025-06-04 14:57:01 -0300

    test: ensure user can only delete his transaction

--- src/use-cases/transaction/delete-transaction.test.js
+++ src/use-cases/transaction/delete-transaction.test.js
@@ -4,34 +4,50 @@ import { transaction } from '../../tests/index.js'
 
 describe('Delete Transaction Use Case', () => {
     const transactionId = faker.string.uuid()
-
+    const user_id = faker.string.uuid()
     class DeleteTransactionRepositoryStub {
         async execute(transactionId) {
-            return { ...transaction, id: transactionId }
+            return { ...transaction, id: transactionId, user_id }
+        }
+    }
+    class GetTransactionByIdRepositoryStub {
+        async execute() {
+            return { ...transaction, id: transactionId, user_id }
         }
     }
 
     const makeSut = () => {
         const deleteTransactionRepository =
             new DeleteTransactionRepositoryStub()
-        const sut = new DeleteTransactionUseCase(deleteTransactionRepository)
 
-        return { sut, deleteTransactionRepository }
+        const getTransactionByIdRepository =
+            new GetTransactionByIdRepositoryStub()
+
+        const sut = new DeleteTransactionUseCase(
+            deleteTransactionRepository,
+            getTransactionByIdRepository,
+        )
+
+        return {
+            sut,
+            deleteTransactionRepository,
+            getTransactionByIdRepository,
+        }
     }
 
     it('should delete transaction successfully', async () => {
         const { sut } = makeSut()
 
-        const result = await sut.execute(transactionId)
+        const result = await sut.execute(transactionId, user_id)
 
-        expect(result).toEqual({ ...transaction, id: transactionId })
+        expect(result).toEqual({ ...transaction, id: transactionId, user_id })
     })
 
     it('should call DeleteTransactionRepository with correct params', async () => {
         const { sut, deleteTransactionRepository } = makeSut()
         const executeSpy = jest.spyOn(deleteTransactionRepository, 'execute')
 
-        await sut.execute(transactionId)
+        await sut.execute(transactionId, user_id)
 
         expect(executeSpy).toHaveBeenCalledWith(transactionId)
     })
@@ -43,7 +59,7 @@ describe('Delete Transaction Use Case', () => {
             'execute',
         ).mockRejectedValueOnce(new Error())
 
-        const promise = sut.execute(transactionId)
+        const promise = sut.execute(transactionId, user_id)
 
         expect(promise).rejects.toThrow()
     })
