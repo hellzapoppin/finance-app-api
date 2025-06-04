77bc5593608b401da2831c29863e63f20699d7b7
José Leonardo <joseleonaordo85@gnail.com>
2025-06-04 14:56:43 -0300

    feat: ensure user can only delete his transaction

--- src/factories/controllers/transaction.js
+++ src/factories/controllers/transaction.js
@@ -80,8 +80,12 @@ export const makeDeleteTransactionController = () => {
     const deleteTransactionRepository =
         new PostgresDeleteTransactionRepository()
 
+    const getTransactionByIdRepository =
+        new PostgresGetTransactionByIdRepository()
+
     const deleteTransactionUseCase = new DeleteTransactionUseCase(
         deleteTransactionRepository,
+        getTransactionByIdRepository,
     )
 
     const deleteTransactionController = new DeleteTransactionController(
